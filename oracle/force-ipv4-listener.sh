0a089bc391443930279bff43264909cb9202d634
MarkoDojkic <marko.dojkic@gmail.com>
2026-09-16 23:19:48 +0200

    fix: oracle ORA-12514

--- oracle/force-ipv4-listener.sh
+++ oracle/force-ipv4-listener.sh
@@ -4,4 +4,9 @@ if [ -f "$listener_file" ]; then
     sed -E -i 's/HOST[[:space:]]*=[[:space:]]*[^)]*/HOST=0.0.0.0/g' "$listener_file"
     lsnrctl stop LISTENER >/dev/null 2>&1 || true
     lsnrctl start LISTENER >/dev/null
+    sleep 5
+    sqlplus -s / as sysdba <<'SQL'
+ALTER SYSTEM REGISTER;
+EXIT;
+SQL
 fi
